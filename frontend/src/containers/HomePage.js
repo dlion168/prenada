25d76bef74c117b9aadea2c9e508f533e49bf60c
Ashu <e841018@gmail.com>
2022-11-27 00:49:06 +0800

    re-merge homepage

--- frontend/src/containers/HomePage.js
+++ frontend/src/containers/HomePage.js
@@ -8,20 +8,21 @@ import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 const styles = StyleSheet.create({
     body: {
-        backgroundColor: '#FFFFFF', // gray/50
-    },
-    block: {
-        margin: 24,
-    },
-    pad: {
+        backgroundColor: '#F9FAFB', // gray/50
         paddingTop: 8,
         paddingBottom: 8,
+        paddingLeft: 24,
+        paddingRight: 24,
+    },
+    block: {
+        marginTop: 8,
+        marginBottom: 8,
     },
     titleRow: {
         display: 'flex',
         flexDirection: 'row',
-        paddingTop: 8,
-        paddingBottom: 8,
+        marginTop: 8,
+        marginBottom: 8,
     },
     title: {
         fontWeight: 'bold',
@@ -31,18 +32,38 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: 18,
         color: '#F87171',
-        display: 'flex',
     },
-    checklist: {
+    pad: {
+        marginTop: 8,
+        marginBottom: 8,
+    },
+    padCenter: {
+        marginTop: 8,
+        marginBottom: 8,
         display: 'flex',
-        flexDirection: 'column',
+        flexDirection: 'row',
         alignItems: 'center',
-        padding: '0px',
-        position: 'absolute',
-        width: '327px',
-        height: '424px',
-        left: '24px',
-        top: '128px',
+        justifyContent: 'center'
+    },
+    rowFlex: {
+        display: 'flex',
+        flexDirection: 'row',
+        marginTop: 8,
+        marginBottom: 8,
+    },
+    taskCardFlex: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#F9FAFB', // gray/50
+        borderRadius: 10,
+        overflow: 'hidden',
+        justifyContent: 'space-between',
+    },
+    label: {
+        flex: 1,
+        fontSize: 14,
+        color: '#1F2937', // gray/800
     },
 })
 
@@ -50,60 +71,67 @@ const HomePage = ({ navigation }) => {
     const { checkListData, setCheckListData } = useCheckList()
     const [ displayWeek, setDisplayWeek ] = useState(0);
     return (
-        <ScrollView style={styles.body}>
-            <NavBar centerText='w0' rightIcon='bell-s' weekOnChange={
+        <>
+            <NavBar centerText='w1' rightIcon='bell-s' weekOnChange={
                 (week) => { useEffect(() => { setDisplayWeek(week) }) }
             } />
-            <View style={styles.block} >
-                <View style={styles.titleRow} >
-                    <Text style={styles.title} >{
-                        displayWeek === 0 ?
-                            'Trying to conceive (TTC)' :
-                            `Week ${displayWeek} of Pregnency`
-                    }</Text>
-                    <View style={{ flex: 1 }} />
-                    <TouchableOpacity onPress={() => navigation.jumpTo('Checklist')}>
-                        <Text style={styles.titleMore} >See All</Text>
-                    </TouchableOpacity>
-                </View>
-                { (checkListData[displayWeek].data.length > 4 ? 
-                  checkListData[displayWeek].data.slice(0, 4) : checkListData[displayWeek].data).map((obj, idx) =>
-                    <View style={styles.pad} key={idx}>
-                        <ChecklistItem
-                            id={idx}
-                            checked={obj.checked}
-                            text={obj.text}
-                            liked={obj.liked}
-                        />
+            <ScrollView style={styles.body}>
+                <View style={styles.block} >
+                    <View style={styles.titleRow} >
+                        <Text style={styles.title} >{
+                            displayWeek === 0 ?
+                                'Trying to conceive (TTC)' :
+                                `Week ${displayWeek} of Pregnency`
+                        }</Text>
+                        <View style={{ flex: 1 }} />
+                        <TouchableOpacity onPress={() => navigation.jumpTo('Checklist')}>
+                            <Text style={styles.titleMore} >See All</Text>
+                        </TouchableOpacity>
                     </View>
-                )}
-                <View style={[styles.pad, {display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}]}>
-                    {checkListData[displayWeek].data.length > 4 ?  
-                    <FontAwesome5 name ="ellipsis-v" size = {18} color = 'grey' solid />:<></>}
+                    {checkListData[displayWeek].data.slice(0, 4).map((obj, idx) =>
+                        <View style={styles.pad} key={idx}>
+                            <ChecklistItem
+                                week={displayWeek}
+                                idx={idx}
+                                checked={obj.checked}
+                                text={obj.text}
+                                liked={obj.liked}
+                            />
+                        </View>
+                    )}
+                    {checkListData[displayWeek].data.length > 4 ?
+                        <View style={styles.padCenter}>
+                            <FontAwesome5 name ="ellipsis-v" size={18} color='#E5E7EB' /* gray/200 */ solid />
+                        </View> :
+                        <></>
+                    }
+                    <View style={styles.pad}>
+                        <AddListItem onAddHandler={() => {}}/>
+                    </View>
                 </View>
-                <View style={styles.pad}>
-                    <AddListItem onAddHandler={()=>{}}/>
+                <View style={styles.block} >
+                    <View style={styles.titleRow} >
+                        <Text style={styles.title} >Body Data</Text>
+                        <View style={{ flex: 1 }} />
+                        <TouchableOpacity onPress={() => navigation.jumpTo('Body Data')}>
+                            <Text style={styles.titleMore} >See All</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
-            </View>
-            <View style={styles.block} >
-                <View style={styles.titleRow} >
-                    <Text style={styles.title} >Body Data</Text>
-                    <View style={{ flex: 1 }} />
-                    <TouchableOpacity onPress={() => navigation.jumpTo('Body Data')}>
-                        <Text style={styles.titleMore} >See All</Text>
-                    </TouchableOpacity>
+                <View style={styles.block} >
+                    <View style={styles.titleRow} >
+                        <Text style={styles.title} >Tips to you</Text>
+                        <View style={{ flex: 1 }} />
+                        <TouchableOpacity onPress={() => navigation.jumpTo('Library')}>
+                            <Text style={styles.titleMore} >See All</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.rowFlex} >
+
+                    </View>
                 </View>
-            </View>
-            <View style={styles.block} >
-                <View style={styles.titleRow} >
-                    <Text style={styles.title} >Tips to you</Text>
-                    <View style={{ flex: 1 }} />
-                    <TouchableOpacity onPress={() => navigation.jumpTo('Library')}>
-                        <Text style={styles.titleMore} >See All</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-        </ScrollView>
+            </ScrollView>
+        </>
     )
 }
 export default HomePage;
